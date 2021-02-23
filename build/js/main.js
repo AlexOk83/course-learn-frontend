d8c7d46b52eba8283d71307e0e8d271ea27f0b5d
Alexander Okatyev <Alex_ok83@mail.ru>
2021-02-23 23:26:02 +0400

    home page

--- build/js/main.js
+++ build/js/main.js
@@ -2,6 +2,54 @@ jQuery(document).ready(function () {
 
     var $ = jQuery, $doc = $(document), $win = $(window);
 
+    $('.home-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dotsEach: false,
+        autoplay: false,
+        smartSpeed: 2000,
+        navigation: true,
+        navText: [],
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            766:{
+                items:2,
+                nav:true
+            },
+            1024:{
+                items:2,
+                nav:true
+            }
+        },
+    });
+
+    $('.experts-slider').owlCarousel({
+        loop: true,
+        nav: true,
+        dotsEach: false,
+        autoplay: false,
+        smartSpeed: 2000,
+        navigation: true,
+        navText: [],
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            766:{
+                items:2,
+                nav:true
+            },
+            1024:{
+                items:3,
+                nav:true
+            }
+        },
+    });
+
 });
 
 
